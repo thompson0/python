922eb32cc535839e881daefad4f701c2df0bcf69
thompson0 <thompsonjooj@gmail.com>
2025-02-23 01:29:06 -0300

    continuacao

--- oo-sabor-express/app.py
+++ oo-sabor-express/app.py
@@ -1,15 +1,18 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+
 
 restaurante_praca = Restaurante('praça','Gourmet')
-#restaurante_mexicano = Restaurante('Mexican Food','Mexicana')
-#restaurante_japones = Restaurante('Japa','Japones')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-
-
+bebida_suco = Bebida('suco de melancia', 5.0 , 'grande')
+bebida_suco.aplicar_desconto()
+prato_paozinho = Prato('Paozinho', 2.0 , 'O melhor da cidade')
+prato_paozinho.aplicar_desconto()
+restaurante_praca.adiconar_no_cardapio(bebida_suco)
+restaurante_praca.adiconar_no_cardapio(prato_paozinho)
 
 def main():
-    Restaurante.listar_restaurantes()
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
