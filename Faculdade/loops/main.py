bd3082fdcef38c3dd2f58b5c4cc62594cdba3e84
thompson0 <thompsonjooj@gmail.com>
2025-04-25 19:23:33 -0300

    loops

---
+++ Faculdade/loops/main.py
@@ -0,0 +1,27 @@
+# lista = [1,2,3,4,5,6,7,8,9,10]
+
+# for album in lista:
+#     print(album, end=", ")
+
+# for i in range(0, 30):
+#     print(i)
+
+# if 1 in lista:
+#     print('1 is in the list')
+    
+# if 33 not in lista:
+#     print('33 not in the list')    
+    
+
+# for i, c in enumerate(lista):
+#     print(i, ':', c)
+
+# for i in range(0,30):
+#     if i == 15:
+#         break
+#     print(i)
+
+# for i in range (0,30):
+#     if i%2  != 0:
+#         continue
+#     print(i)
---
+++ Faculdade/loops/zip.py
@@ -0,0 +1,4 @@
+a = ('John' ,'Charles','Mike')
+b = ('Jenny','Christy','Monica')
+x = zip(a,b)
+print(list(x))
