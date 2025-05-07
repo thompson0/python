b6874c7ca7433349f16593622eea7b6f63063285
thompson0 <thompsonjooj@gmail.com>
2025-05-06 22:28:26 -0300

    funcoes eval exec e recursivas

---
+++ Faculdade/funçoess/eval_exec.py
@@ -0,0 +1,30 @@
+# expressao = "2 + 3 * 5"
+
+# resultado = eval(expressao)
+
+# print(resultado)
+
+# x = 5
+# y = 10
+
+# expressao2 = "x * y + 2"
+
+# resultado2 = eval(expressao2,{},{"x": x, "y": y})
+
+# print(resultado2)
+
+# def minha_funcao():
+#    global x
+#    x = 3 
+    
+# minha_funcao()
+# print(x)
+    
+# code_str = """
+# result = []
+# for i in range(10):
+#    if i % 2 == 0:
+#       result.append(i)
+#       print(result)
+# """
+# exec(code_str)
---
+++ Faculdade/funçoess/recursivas.py
@@ -0,0 +1,25 @@
+# def fatorial(n):
+#     if n < 2:
+#         return 1
+    
+#     return n * fatorial(n - 1)
+
+# print(fatorial(10))    
+
+# def multiplicacao(a, b):
+#     if a == 0 or b == 0:
+#         return 0
+#     if a == 1:
+#         return b
+#     return a + multiplicacao(a, b - 1)
+
+# print(multiplicacao(4, 1)) 
+
+# def contagem(n):
+#     if n == 0:
+#         return 0
+#     print(n)
+#     return contagem(n - 1) 
+# print(contagem(9))
+
+
